01d909e1a20b02c61959ef8422b4e43cfc5c6001
5tepan <f.stepa.n@yandex.ru>
2026-07-30 09:53:58 +0300

    filament управление пользователями

---
+++ blog/app/Filament/Resources/Users/Pages/EditUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Users\Pages;
+
+use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUser extends EditRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     * @return array<string, mixed>
+     */
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        unset($data['password']);
+
+        return $data;
+    }
+}
