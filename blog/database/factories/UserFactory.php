2b1498e9648184c910337ebf594b3b95af39518a
5tepan <f.stepa.n@yandex.ru>
2026-07-30 11:03:33 +0300

    seeder для админа

--- blog/database/factories/UserFactory.php
+++ blog/database/factories/UserFactory.php
@@ -29,17 +29,22 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'user',
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
         ]);
     }
 }
